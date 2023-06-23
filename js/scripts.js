e38f2c253cbdd2cafa790909dba7c91c7ee6b5eb
matt-visentin <134010450+matt-visentin@users.noreply.github.com>
2023-06-23 17:11:43 +0200

    Added app functionalities

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function newItem(){
+    // 1. Adding a new item to the list of items
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
+
+    if (inputValue === ""){
+        alert("You must write something!");
+    } else {
+        $("#list").append(li);
+    };
+
+    // // 2. Crossing out an item from the list of items
+
+    li.on("dblclick", function crossOut(){
+        li.toggleClass("strike");
+    });
+
+    // // 3. Adding the delete button
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton);
+    crossOutButton.on("click", function deleteListItem(){
+        li.addClass(".delete");
+    })
+
+    // // 4. Reordening the items
+    $("#list").sortable();
+}
